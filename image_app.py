d47b5d072931dabf806abde4501ae66160785ddf
zaniarkarimi <zaniarkarimi@chmail.ir>
2025-03-02 21:06:15 +0330

    Add imports

---
+++ image_app.py
@@ -0,0 +1,2 @@
+import cv2import numpy as npimport matplotlib.pyplot as pltfrom tkinter import Tk, filedialog, Button, Label, Frame, Canvas, messagebox, ttk, Menu, PhotoImage, Toplevel, StringVarfrom PIL import Image, ImageTkfrom scipy.fft import fft2, fftshiftfrom scipy.stats import skew, kurtosisimport loggingimport osfrom matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
